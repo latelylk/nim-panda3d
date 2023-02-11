24e3be7541fcf2636b79b59b8fff2862fa21d2f6
Pierce <33592564+latelylk@users.noreply.github.com>
2023-02-11 18:20:37 -0500

    upd: improve solar system samples

--- samples/solar-system/step1_blank_window.nim
+++ samples/solar-system/step1_blank_window.nim
@@ -1,13 +1,14 @@
 # Original Python implementation by Shao Zhang and Phil Saltzman
-# Author: 
+# Adapted to Nim by:
 # Last Updated: 2022-10-15
 #
 # This tutorial is intended as a initial panda scripting lesson going over
 # display initialization, loading models, placing objects, and the scene graph.
 #
-# Step 1: ShowBase contains the main Panda3D modules. Importing it
-# initializes Panda and creates the window. The run() command causes the
-# real-time simulation to begin
+# Step 1: ShowBase contains the main Panda3D modules. Calling ShowBase()
+# initializes Panda and openDefaultWindow() creates the window. The run()
+# command causes the real-time simulation to begin
+
 import nimpanda3d/direct/showbase
 
 var base = ShowBase() # The ShowBase constructor doesn't open a window.
--- samples/solar-system/step2_basic_setup.nim
+++ samples/solar-system/step2_basic_setup.nim
@@ -1,40 +1,44 @@
 # Original Python implementation by Shao Zhang and Phil Saltzman
-# Author: 
+# Adapted to Nim by:
 # Last Updated: 2022-10-24
 #
 # This tutorial is intended as a initial panda scripting lesson going over
 # display initialization, loading models, placing objects, and the scene graph.
 #
-# Step 2: After initializing panda, we define a class called World. We put
-# all of our code in a class to provide a convenient way to keep track of
-# all of the variables our project will use, and in later tutorials to handle
-# keyboard input.
-# The code contained in the __init__ method is executed when we instantiate
-# the class (at the end of this file).  Inside __init__ we will first change
+# Step 2: After initializing panda, we define an object called World. We use
+# the World object to provide a convenient way to keep track of all of the
+# variables our project will use, and in later tutorials to handle keyboard
+# input.
+# The code contained in the newWorld() method is executed when we call
+# the function (at the end of this file). Inside newWorld() we will first change
 # the background color of the window.  We then disable the mouse-based camera
 # control and set the camera position.
+
 import nimpanda3d/direct/showbase
 import nimpanda3d/panda3d/core  # Contains most of Panda's modules
 import nimpanda3d/direct/gui # Imports Gui objects we use for putting
-# text on the screen
-import std/options
+import std/options # Used for putting text on the screen
+
+# Define World object and variables we want to use in the scene
+type World = ref object of DirectObject
+    title: OnscreenText
 
 # Initialize Panda and create a window
 var base = ShowBase()
 base.openDefaultWindow() # open the viewing window
 
-proc newWorld =  # The initialization method for creating the World object
-    # result is an implicit return variable of nim functions
-    # var result = new World
+# The initialization method for creating the World object
+proc newWorld: World =
+    var self = World() # Create a new World object and assign it to self
 
     # Create some text overlayed on our screen.
     # We will use similar commands in all of our tutorials to create titles and
     # instruction guides.
-    discard newOnscreenText(text = "Panda3D: Tutorial 1 - Solar System",
+    self.title = newOnscreenText(text = "Panda3D: Tutorial 1 - Solar System",
                                 style = plain,
-                                pos = (-0.1f, 0.1f),
+                                pos = (-0.1, 0.1),
                                 scale = option[LVecBase2]((0.07, 0.07)),
-                                fg = option[LVecBase4]((1f, 1f, 1f, 1f)),
+                                fg = option[LVecBase4]((1, 1, 1, 1)),
                                 align = ord(TextProperties.A_right),
                                 parent = option[NodePath](base.a2dBottomRight))
 
@@ -52,11 +56,12 @@ proc newWorld =  # The initialization method for creating the World object
 
     # Set the camera orientation (heading, pitch, roll) in degrees
     base.camera.setHpr(0, -90, 0)
-# end class world
 
-# Now that our class is defined, we create an instance of it.
-# Doing so calls the __init__ method set up above
-newWorld()
+    # return our newly minted world
+    return self
+
+# Now that our procedure is defined, we can call it to create the World object
+let w = newWorld()
 
 # As usual - run() must be called before anything can be shown on screen
 base.run()
